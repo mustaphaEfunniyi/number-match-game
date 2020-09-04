5cc84a0ecc473c83914b48ea853380448d887119
Efunniyi Mustapha <mefunniyi@yahoo.com>
2020-09-05 00:21:37 +0100

    Added the Image generation logic

---
+++ app.js
@@ -0,0 +1,121 @@
+const images = [
+    {
+      image_name: 'bananas.jpg',
+      number_of_items: 6,
+    },
+    {
+      image_name: 'birthday candles.jpg',
+      number_of_items: 7,
+    },
+    {
+      image_name: 'blocks.jpg',
+      number_of_items: 6,
+    },
+    {
+      image_name: 'brushes.jpg',
+      number_of_items: 7,
+    },
+    {
+      image_name: 'cakes.jpg',
+      number_of_items: 7,
+    },
+    {
+      image_name: 'cars.jpg',
+      number_of_items: 2,
+    },
+    {
+      image_name: 'crayons.jpg',
+      number_of_items: 8,
+    },
+    {
+      image_name: 'cupcakes.jpg',
+      number_of_items: 7,
+    },
+    {
+      image_name: 'deer.jpg',
+      number_of_items: 3,
+    },
+    {
+      image_name: 'donuts.jpg',
+      number_of_items: 6,
+    },
+    {
+      image_name: 'ducks.jpg',
+      number_of_items: 6,
+    },
+    {
+      image_name: 'eggs.jpg',
+      number_of_items: 8,
+    },
+    {
+      image_name: 'elephants.jpg',
+      number_of_items: 7,
+    },
+    {
+      image_name: 'hot air balloons.jpg',
+      number_of_items: 5,
+    },
+    {
+      image_name: 'jelly beans.jpg',
+      number_of_items: 9,
+    },
+    {
+      image_name: 'macaroons.jpg',
+      number_of_items: 7,
+    },
+    {
+      image_name: 'pencils.jpg',
+      number_of_items: 12,
+    },
+    {
+      image_name: 'people.jpg',
+      number_of_items: 6,
+    },
+    {
+      image_name: 'peppers.jpg',
+      number_of_items: 2,
+    },
+    {
+      image_name: 'pizza slices.jpg',
+      number_of_items: 8,
+    },
+  ]
+
+const button = document.querySelector('#play-button');
+
+const generate = () => {
+  if (images.length === 0) {
+    stopTimer();
+    return;
+  }
+  
+  const randomNumber = Math.floor(Math.random() * images.length);
+  const randomImageName= images[randomNumber].image_name;
+  console.log(randomImageName);
+  const imageContainer = document.getElementById('imageContainer');
+
+  if (imageContainer.hasChildNodes()) {
+    imageContainer.removeChild(imageContainer.firstChild);
+  }
+
+  const image = document.createElement('img');
+  image.src = `img/${randomImageName}`;
+  imageContainer.appendChild(image);
+  images.splice(randomNumber, 1);
+}
+
+let timeRef;
+const timer = () => {
+  timeRef = setInterval(generate, 200);
+}
+
+const stopTimer = () => {
+  clearInterval(timeRef);
+}
+
+const play = () => {
+  generate();
+  timer();
+}
+
+button.addEventListener('click', play);
