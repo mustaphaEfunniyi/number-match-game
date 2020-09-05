c40f8011d3caed17de00b85dd9240b8ed012a7d4
Efunniyi Mustapha <mefunniyi@yahoo.com>
2020-09-05 03:17:23 +0100

    Added game logic

--- app.js
+++ app.js
@@ -81,7 +81,22 @@ const images = [
     },
   ]
 
+let currentImageValue = 0,
+displayNumber = 0,
+score = 0,
+totalAvailable = images.length;
+chosen = false;
+
+const timeDelay = 3000;
+
+document.getElementById('statsContent').style.visibility = "hidden";
+document.getElementById('currentScore').innerHTML = score;
+document.getElementById('totalAvailable').innerHTML = totalAvailable;
+document.getElementById('timeSetting').innerHTML = timeDelay / 1000;
+
 const button = document.querySelector('#play-button');
+const matchButton = document.querySelector('.matchButton');
+const noMatchButton = document.querySelector('.noMatchButton');
 
 const setImageSrc= (randomImageName) => {
   const imageContainer = document.getElementById('imageContainer');
@@ -110,20 +125,25 @@ const generateDisplayNumber = (numberOfItems, plusOrMinus) => {
   if (split === 0) {
     // display real number
     document.getElementById('number').innerHTML = numberOfItems;
+    displayNumber = numberOfItems;
   } else {
     // display one higher or one lower
     document.getElementById('number').innerHTML = `${
       numberOfItems + plusOrMinus
     }`;
+    displayNumber = numberOfItems + plusOrMinus;
   }
+  currentImageValue = numberOfItems;
 }
 
 const generate = () => {
   if (images.length === 0) {
+    endOfGame();
     stopTimer();
     return;
   }
 
+  chosen = false;
   const randomNumber = Math.floor(Math.random() * images.length);
   const randomImageName= images[randomNumber].image_name;
   setImageSrc(randomImageName);
@@ -137,7 +157,7 @@ const generate = () => {
 
 let timeRef;
 const timer = () => {
-  timeRef = setInterval(generate, 2000);
+  timeRef = setInterval(generate, timeDelay);
 }
 
 const stopTimer = () => {
@@ -145,8 +165,44 @@ const stopTimer = () => {
 }
 
 const play = () => {
+  document.getElementById('message').style.display = "none";
+  document.getElementById('startScreen').style.display = "none";
+  document.getElementById('play-button').style.display = "none";
+  document.getElementById('statsContent').style.visibility = "visible";
+  
   generate();
   timer();
 }
 
+const endOfGame = () => {
+  document.getElementById('statsContent').style.visibility = "hidden";
+  document.getElementById('message').style.display = "block";
+  document.getElementById('imageContainer').style.display = "none";
+  document.getElementById('statsContent').style.display = "none";
+  document.getElementById(
+    'message'
+    ).innerHTML = `Game Over, your score was ${score} / ${totalAvailable}`;
+  
+  
+  setTimeout(() => location.reload(), 4000);
+}
+
+const match = () => {
+  if (!chosen) {
+    currentImageValue === displayNumber ? score++ : score--;
+    chosen = true;
+    document.getElementById('currentScore').innerHTML = score;
+  }
+}
+
+const noMatch = () => {
+  if (!chosen) {
+    currentImageValue !== displayNumber ? score++ : score--;
+    chosen = true;
+    document.getElementById('currentScore').innerHTML = score;
+  }
+}
+
 button.addEventListener('click', play);
+matchButton.addEventListener('click', match);
+noMatchButton.addEventListener('click', noMatch);
